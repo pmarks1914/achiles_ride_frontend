c544026b162bd17c4e222e00c8f15877127ebb7e
Patrick <pmarks1914@gmail.com>
2025-02-28 23:24:35 +0000

    login

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -83,10 +83,6 @@ export function SignIn() {
               </a>
             </Typography>
           </div>
-          <div className="space-y-4 mt-8">
-            
-            
-          </div>
           <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
             Not registered?
             <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
