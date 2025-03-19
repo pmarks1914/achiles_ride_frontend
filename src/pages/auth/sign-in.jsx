39b9cc3210e9708e4aacf25146dc22f2ed80716c
Patrick <pmarks1914@gmail.com>
2025-03-19 23:57:41 +0000

    sign call api

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -11,8 +11,128 @@ import { Link } from "react-router-dom";
 export function SignIn() {
 
   // call api
-  function callSignIn(){
-    
+  const [getFormDataError, setGetFormDataError] = React.useState({
+    "password": false,
+    "email": false
+  })
+  const [usernameVar, setUsernameVar] = useState("")
+  const [passwordVar, setPasswordVar] = useState("")
+  const [loader, setLoader] = useState("<div></div")
+  const [login, setLogin] = useState("Login")
+  const [loginError, setLoginError] = useState("")
+  const [userType, setUserType] = useState("Student")
+
+  function CheckLogin(e){
+    e.preventDefault();    
+
+    // console.log("fff", process.env.REACT_APP_BASE_API, passwordVar, usernameVar)
+    if (usernameVar === "") {
+      setGetFormDataError({ ...getFormDataError, ...{ "email": true } })
+    }
+    else if (passwordVar === "") {
+      setGetFormDataError({ ...getFormDataError, ...{ "password": true } })
+
+    }
+    else {
+      setLogin("")
+      setLoader('<div class="spinner-border "style="color: #e0922f;"></div>`')
+      // console.log(" login ")
+      // let data = JSON.stringify({
+      //   "username": usernameVar,
+      //   "password": passwordVar
+      // });
+
+      const formData = new FormData();
+      formData.append('username', usernameVar)
+      formData.append('password', passwordVar)
+
+      let config = {
+        method: 'post',
+        url: process.env.REACT_APP_BASE_API + "/login/",
+        maxBodyLength: Infinity,
+        headers: { 
+          'Content-Type': 'application/x-www-form-urlencoded'      
+        },
+        data: formData
+      };
+      axios.request(config).then(response => {
+        // console.log(response.data, "auth ", response.data.token_type + " " + response.data.token);
+
+        let config_2 = {
+          method: 'get',
+          url: process.env.REACT_APP_BASE_API + "/user-details/",
+          maxBodyLength: Infinity,
+          headers: { 
+            'Authorization': 'Bearer ' + response?.data?.access_token
+          } 
+        };
+        axios.request(config_2).then(response_2 => {
+          // console.log(response_2);
+          if(response_2?.status === 200){
+              let permission_list = ['MERCHANT_ADMIN', 'SUPER_ADMIN']
+              let counter = 600000; // 600000 = 10m
+              let userData = response_2?.data;
+              userData = {...userData, ...response?.data, ...{type: userType, counter: counter}, ...{permission_list: permission_list}}
+              // console.log(userData);
+              localStorage.setItem("userDataStore", JSON.stringify(userData));
+              // Cookie
+              // document.cookie = "cookieData" + "=" + JSON.stringify({ 
+              //   account: "", 
+              // wallet: "",
+              // status: "",
+              // access: "",
+              // refresh: "",            
+              // permission_list: ""
+              // })
+
+            if(response_2?.data?.user?.must_change_password){
+              window.location.href = "/change-password";
+            }
+            else{
+              setTimeout(() => {
+                window.location.href = "/dashboard";
+              }, 1000)
+            }
+
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      }).catch(function (error) {
+
+        if (error.response) {
+          // console.log("==>", error.response?.data?.detail);
+
+          setLoader("<a></a>")
+          setLogin("Login")
+          setLoginError(`Wrong user credentials: ${error?.response?.data?.detail}`)
+          /*
+            * The request was made and the server responded with a
+            * status code that falls out of the range of 2xx
+            */
+
+        } else if (error.request) {
+
+          setLoader("<a></a>")
+          setLogin("Login")
+          setLoginError("Wrong user credentials")
+          /*
+            * The request was made but no response was received, `error.request`
+            * is an instance of XMLHttpRequest in the browser and an instance
+            * of http.ClientRequest in Node.js
+            */
+
+        } else {
+          // Something happened in setting up the request and triggered an Error
+
+        }
+      }
+      );
+
+    }
+
   }
   return (
     <section className="m-8 flex gap-4">
