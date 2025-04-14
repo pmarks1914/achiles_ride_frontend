1df263a665463c65d0433db674f7b8ede939c6c0
Patrick <pmarks1914@gmail.com>
2025-04-14 23:01:44 +0000

    session

--- src/layouts/auth.jsx
+++ src/layouts/auth.jsx
@@ -35,9 +35,8 @@ export function Auth() {
   window.onstorage = () => {
     console.log("storage ")
     let userDataStore = JSON.parse(localStorage.getItem("userDataStore"));
-    if(userDataStore?.access_token){ }
-    else if(window.location.href.startsWith("/dashboard/")){
-      window.location.href="/auth/sign-in";
+    if (!userDataStore?.access_token && window.location.pathname.startsWith("/dashboard")) {
+      window.location.href = "/auth/sign-in";
     }
   };
 
